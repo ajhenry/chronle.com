79f3a5babf2d61f362f66a32bd04723432a88815
Andrew Henry <andrew@ajhenry.dev>
2024-06-17 22:02:21 -0400

    fix: overflow on mobile

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -4,7 +4,7 @@ import { ReactNode } from "react";
 
 export default function MainLayout({ children }: { children: ReactNode }) {
   return (
-    <div className="flex flex-col min-h-screen animate-in fade-in">
+    <div className="flex flex-col min-h-screen animate-in fade-in overflow-x-hidden">
       <NavBar />
       <div className="flex flex-col grow h-full">{children}</div>
       <Footer />
